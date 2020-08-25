520a9e1350e1cc8d68c95382f50aee507aa69a33
frescobar <frescobar87@gmail.com>
2020-08-24 22:47:15 -0500

    Added routes

--- index.js
+++ index.js
@@ -10,5 +10,11 @@ app.get("/", (req, res) => {
   res.send("request to the homepage");
 });
 
+//Defining routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`listening on port port ${PORT}`));
